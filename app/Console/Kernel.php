d345aaa446ab2dfbe8e39b79fd96b58f4ef39a06
Marcel Hauri <marcel@hauri.me>
2018-08-14 16:05:16 +0200

    [fix] cron did run too early

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('magento:generate:statistics', ['--online'])->everyMinute();
+        $schedule->command('magento:generate:statistics', ['--online'])->everyFifteenMinutes();
     }
 
     /**
