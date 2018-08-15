f7c970c9441a557307b79d3aa1d7daf9ca84d6df
Marcel Hauri <marcel@hauri.me>
2018-08-15 14:23:31 +0200

    [task] try sections

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
     <div id="app" class="fullpage">
-        <div class="section">
+        <div class="section first">
             <h2>Total Pull Requests in {{ \Carbon\Carbon::now()->yearIso }}<sup><span class="small">*</span></sup></h2>
             <repository-chart repo="pullrequests" year="2018"></repository-chart>
             <span class="small">*for all Magento Community Projects</span>
         </div>
-        <div class="section">
+        <div class="section second">
             <h2>Total Issues in {{ \Carbon\Carbon::now()->yearIso }}<sup><span class="small">*</span></sup></h2>
             <repository-chart repo="issues" year="2018"></repository-chart>
             <span class="small">*for all Magento Community Projects</span>
         </div>
-        <div class="section">Section 3</div>
+        <div class="section third">Section 3</div>
     </div>
 @endsection
